1f8e4b40c98eae4523b1cecafcd4dc462fe91536
mohammad alameri <onex911@hotmail.com>
2022-01-13 13:58:37 +0200

    edit

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import Display from "./Display";
 import Keys from "./Keys";
 import { Output } from "./Output";
